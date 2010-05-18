e6381a2799d8135c27ca8fe405395edfd5613472
Michel Boyer de la Giroday <michel.giroday@kdab.com>
2010-05-18 13:36:58 +0000

    implemented a sugaraccount class as a first step to handle the account data
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@35058 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ kdcrmdata/sugaraccount.cpp
@@ -0,0 +1,544 @@
+#include "sugaraccount.h"
+
+#include <QtCore/QSharedData>
+
+class SugarAccount::Private : public QSharedData
+{
+  public:
+    Private()
+      : mEmpty( true )
+    {
+
+    }
+
+    Private( const Private &other )
+      : QSharedData( other )
+    {
+      mEmpty = other.mEmpty;
+
+      mId = other.mId;
+      mName = other.mName;
+      mWebsite = other.mWebsite;
+      mTyckerSymbol = other.mTyckerSymbol;
+      mMemberOf = other.mMemberOf;
+      mOwnership = other.mOwnership;
+      mIndustry = other.mIndustry;
+      mType = other.mType;
+      mCampaign = other.mCampaign;
+      mAssignedTo = other.mAssignedTo;
+      mOfficePhone = other.mOfficePhone;
+      mFax = other.mFax;
+      mOtherPhone = other.mOtherPhone;
+      mEmployees = other.mEmployees;
+      mRating = other.mRating;
+      mSicCode = other.mSicCode;
+      mAnnualRevenue = other.mAnnualRevenue;
+      mBillingAddress = other.mBillingAddress;
+      mCity = other.mCity;
+      mState = other.mState;
+      mPostalCode = other.mPostalCode;
+      mCountry = other.mCountry;
+      mShippingAddress = other.mShippingAddress;
+      mShippingCity = other.mShippingCity;
+      mShippingState = other.mShippingState;
+      mShippingPostalCode = other.mShippingPostalCode;
+      mShippingCountry = other.mShippingCountry;
+      mPrimaryEmail = other.mPrimaryEmail;
+      mOptedOutEmail = other.mOptedOutEmail;
+      mInvalidEmail = other.mInvalidEmail;
+      mDescription = other.Description;
+    }
+
+    bool mEmpty;
+
+    QString mId;
+    QString mName;
+    QString mWebsite;
+    QString mTyckerSymbol;
+    QString mMemberOf;
+    QString mOwnership;
+    QString mIndustry;
+    QString mType;
+    QString mCampaign;
+    QString mAssignedTo;
+    QString mOfficePhone;
+    QString mFax;
+    QString mOtherPhone;
+    QString mEmployees;
+    QString mRating;
+    QString mSicCode;
+    QString mAnnualRevenue;
+    QString mBillingAddress;
+    QString mCity;
+    QString mState;
+    QString mPostalCode;
+    QString mCountry;
+    QString mShippingAddress;
+    QString mShippingCity;
+    QString mShippingState;
+    QString mShippingPostalCode;
+    QString mShippingCountry;
+    QString mPrimaryEmail;
+    QString mOptedOutEmail;
+    QString mInvalidEmail;
+    QString mDescription;
+
+};
+
+SugarAccount::SugarAccount()
+  : d( new Private )
+{
+}
+
+
+SugarAccount::SugarAccount( const SugarAccount &other )
+  : d( other.d )
+{
+}
+
+SugarAccount::~SugarAccount()
+{
+}
+
+SugarAccount &SugarAccount::operator=( const SugarAccount &other )
+{
+  if ( this != &other ) {
+    d = other.d;
+  }
+
+  return *this;
+}
+
+bool SugarAccount::operator==( const SugarAccount &other ) const
+{
+    if ( d->mId != other.d->mId )
+        return false;
+    if ( d->mName !=  other.d->mName )
+        return false;
+    if ( d->mWebsite != other.d->mWebsite )
+        return false;
+    if ( d->mTyckerSymbol != other.d->mTyckerSymbol )
+        return false;
+    if ( d->mMemberOf != other.d->mMemberOf )
+        return false;
+    if ( d->mOwnership != other.d->mOwnership )
+        return false;
+    if ( d->mIndustry != other.d->mIndustry )
+        return false;
+    if ( d->mType != other.d->mType )
+        return false;
+    if ( d->mCampaign != other.d->mCampaign )
+        return false;
+    if ( d->mAssignedTo != other.d->mAssignedTo )
+        return false;
+    if ( d->mOfficePhone != other.d->mOfficePhone )
+        return false;
+    if ( d->mFax != other.d->mFax )
+        return false;
+    if ( d->mOtherPhone != other.d->mOtherPhone )
+        return false;
+    if ( d->mEmployees != other.d->mEmployees )
+        return false;
+    if ( d->mRating != other.d->mRating )
+        return false;
+    if ( d->mSicCode != other.d->mSicCode )
+        return false;
+    if ( d->mAnnualRevenue != other.d->mAnnualRevenue )
+        return false;
+    if ( d->mBillingAddress != other.d->mBillingAddress )
+        return false;
+    if ( d->mCity != other.d->mCity )
+        return false;
+    if ( d->mState != other.d->mState )
+        return false;
+    if ( d->mPostalCode != other.d->mPostalCode )
+        return false;
+    if ( d->mCountry != other.d->mCountry )
+        return false;
+    if ( d->mShippingAddress != other.d->mShippingAddress )
+        return false;
+    if ( d->mShippingCity != other.d->mShippingCity )
+        return false;
+    if ( d->mShippingState != other.d->mShippingState )
+        return false;
+    if ( d->mShippingPostalCode != other.d->mShippingPostalCode )
+        return false;
+    if ( d->mShippingCountry != other.d->mShippingCountry )
+        return false;
+    if ( d->mPrimaryEmail != other.d->mPrimaryEmail )
+        return false;
+    if ( d->mOptedOutEmail != other.d->mOptedOutEmail )
+        return false;
+    if ( d->mInvalidEmail != other.d->mInvalidEmail )
+        return false;
+    if ( d->mDescription != other.d->Description )
+        return false;
+
+  return true;
+}
+
+bool SugarAccount::operator!=( const SugarAccount &a ) const
+{
+  return !( a == *this );
+}
+
+bool SugarAccount::isEmpty() const
+{
+  return d->mEmpty;
+}
+
+void SugarAccount::clear()
+{
+  *this = SugarAccount();
+}
+
+void SugarAccount::setId( const QString &id )
+{
+  d->mEmpty = false;
+  d->mId = id;
+}
+
+QString SugarAccount::id() const
+{
+  return d->mId;
+}
+
+void SugarAccount::setName( const QString &name )
+{
+    d->mEmpty = false;
+    d->mName = name;
+}
+
+QString SugarAccount::name() const
+{
+    return d->mName;
+}
+
+void SugarAccount::setWebsite( const QString &value )
+{
+    d->mEmpty = false;
+    d->mWebsite = value;
+}
+
+
+QString SugarAccount::website() const
+{
+    return d->mWebsite;
+}
+
+void SugarAccount::setTyckerSymbol( const QString &value )
+{
+    d->mEmpty = false;
+    d->mTyckerSymbol = value;
+}
+
+QString SugarAccount::tyckerSymbol() const
+{
+    return d->mTyckerSymbol;
+}
+
+void SugarAccount::setMemberOf( const QString &value )
+{
+    d->mEmpty = false;
+    d->mMemberOf = value;
+}
+
+QString SugarAccount::memberOf() const
+{
+    return->d->mMemberOf;
+}
+
+void SugarAccount::setOwnership( const QString &value )
+{
+    d->mEmpty = false;
+    d->mOwnership = value;
+}
+
+QString SugarAccount::ownership() const
+{
+    return d->mOwnership;
+}
+
+void SugarAccount::setIndustry( const QString &value )
+{
+    d->mEmpty = false;
+    d->mIndustry = value;
+}
+
+QString SugarAccount::industry() const
+{
+    return d->mIndustry;
+}
+
+
+void SugarAccount::setType( const QString &value )
+{
+    d->mEmpty = false;
+    d->mType = value;
+}
+
+QString SugarAccount::type() const
+{
+    return d->mType;
+}
+
+void SugarAccount::setCampaign( const QString &value )
+{
+    d->mEmpty = false;
+    d->mCampaign = value;
+}
+
+QString SugarAccount::campaign() const
+{
+    return d->mCampaign;
+}
+
+
+void SugarAccount::setAssignedTo( const QString &value )
+{
+    d->mEmpty = false;
+    d->mAssignedTo = value;
+}
+
+QString SugarAccount::assignedTo() const
+{
+    return d->mAssignedTo;
+}
+
+void SugarAccount::setOfficePhone( const QString &value )
+{
+    d->mEmpty = false;
+    d->mOfficePhone = value;
+}
+
+QString SugarAccount::officePhone() const
+{
+    return d->mOfficePhone;
+}
+
+void SugarAccount::setFax( const QString &value )
+{
+    d->mEmpty = false;
+    d->mFax = value;
+}
+
+QString SugarAccount::fax() const
+{
+    return d->mFax;
+}
+
+
+void SugarAccount::setOtherPhone( const QString &value )
+{
+    d->mEmpty = false;
+    d->mOtherPhone = value;
+}
+
+QString SugarAccount::otherPhone() const
+{
+    return d->otherPhone;
+}
+
+void SugarAccount::setEmployees( const QString &value )
+{
+    d->mEmpty = false;
+    d->mEmployees = value;
+}
+
+QString SugarAccount::employees() const
+{
+    return d->mEmployees;
+}
+
+
+void SugarAccount::setRating( const QString &value )
+{
+    d->mEmpty = false;
+    d->mRating = value;
+}
+
+QString SugarAccount::rating() const
+{
+    return d->mRating;
+}
+
+void SugarAccount::setSicCode( const QString &value )
+{
+    d->mEmpty = false;
+    d->mSicCode = value;
+}
+
+QString SugarAccount::sicCode() const
+{
+    return d->mSicCode;
+}
+
+void SugarAccount::setAnnualRevenue( const QString &value )
+{
+    d->mEmpty = false;
+    d->mAnnualRevenue = value;
+}
+
+QString SugarAccount::annualRevenue() const
+{
+    return d->mAnnualRevenue;
+}
+
+void SugarAccount::setBillingAddress( const QString &value )
+{
+    d->mEmpty = false;
+    d->mBillingAddress = value;
+}
+
+QString SugarAccount::billingAddress() const
+{
+    return d->mBillingAddress;
+}
+
+void SugarAccount::setCity( const QString &value )
+{
+    d->mEmpty = false;
+    d->mCity = value;
+}
+
+QString SugarAccount::city() const
+{
+    return d->mCity;
+}
+
+void SugarAccount::setState( const QString &value )
+{
+    d->mEmpty = false;
+    d->mState = value;
+}
+
+
+QString SugarAccount::state() const
+{
+    return d->mState;
+}
+
+void SugarAccount::setPostalCode( const QString &value )
+{
+    d->mEmpty = false;
+    d->mPostalCode = value;
+}
+
+QString SugarAccount::postalCode() const
+{
+    return d->mPostalCode;
+}
+
+void SugarAccount::setCountry( const QString &value )
+{
+    d->mEmpty = false;
+    d->mCountry = value;
+}
+
+QString SugarAccount::country() const
+{
+    return d->mCountry;
+}
+
+void SugarAccount::setShippingAddress( const QString &value )
+{
+    d->mEmpty = false;
+    d->mShippingAddress = value;
+}
+
+QString SugarAccount::shippingAddress() const
+{
+    d->mShippingAddress;
+}
+
+
+void SugarAccount::setShippingCity( const QString &value )
+{
+    d->mEmpty = false;
+    d->mShippingCity = value;
+}
+
+QString SugarAccount::shippingCity() const
+{
+    return d->mShippingCity;
+}
+
+
+void SugarAccount::setShippingState( const QString &value )
+{
+    d->mEmpty = false;
+    d->mShippingState = value;
+}
+
+QString SugarAccount::shippingState() const
+{
+    return d->mShippingState;
+}
+
+void SugarAccount::setShippingPostalCode( const QString &value )
+{
+    d->mEmpty = false;
+    d->mShippingPostalCode = value;
+}
+
+QString SugarAccount::shippingPostalCode() const
+{
+    return d->mShippingPostalCode;
+}
+
+void SugarAccount::setShippingCountry( const QString &value )
+{
+    d->mEmpty = false;
+    d->mShippingCountry = value;
+}
+
+QString SugarAccount::shippingCountry() const
+{
+    return d->mShippingCountry;
+}
+
+void SugarAccount::setPrimaryEmail( const QString &value )
+{
+    d->mEmpty = false;
+    d->mPrimaryEmail = value;
+}
+
+QString SugarAccount::primaryEmail() const
+{
+    return d->mPrimaryEmail;
+}
+
+void SugarAccount::setOptedOutEmail( const QString &value )
+{
+    d->mEmpty = false;
+    d->mOptedOutEmail = value;
+}
+
+QString SugarAccount::optedOutEmail() const
+{
+    return d->mOptedOutEmail;
+}
+
+
+void SugarAccount::setInvalidEmail( const QString &value )
+{
+    d->mEmpty = false;
+    d->mInvalidEmail = value;
+}
+
+QString SugarAccount::invalidEmail() const
+{
+    return d->mInvalidEmail;
+}
+
+
+void SugarAccount::setDescription( const QString &value )
+{
+    d->mEmpty = false;
+    d->mDescription = value;
+}
+
+QString SugarAccount::description() const
+{
+    return d->mDescription;
+}
---
+++ kdcrmdata/sugaraccount.h
@@ -0,0 +1,364 @@
+#ifndef SUGARACCOUNT_H
+#define SUGARACCOUNT_H
+
+
+#include <QtCore/QSharedDataPointer>
+
+/**
+  @short sugar account entry
+
+  This class represents an account entry in sugarcrm.
+
+  The data of this class is implicitly shared. You can pass this class by value.
+ */
+
+class SugarAccount
+{
+
+  public:
+    /**
+      A list of account objects
+     */
+    typedef AccountList List;
+    /**
+      A map from unique identifier to sugaraccount.
+
+      @see uid()
+     */
+    typedef QMap<QString, SugarAccount> Map;
+
+    /**
+      Construct an empty SugarAccount entry.
+     */
+    SugarAccount();
+
+    /**
+      Destroys the SugarAccount entry.
+     */
+    ~SugarAccount();
+
+    /**
+      Copy constructor.
+     */
+    SugarAccount( const SugarAccount & );
+
+    /**
+      Assignment operator.
+
+      @return a reference to @c this
+    */
+    SugarAccount &operator=( const SugarAccount & );
+
+    /**
+      Equality operator.
+
+      @return @c true if @c this and the given SugarAccount are equal,
+              otherwise @c false
+    */
+    bool operator==( const SugarAccount & ) const;
+
+    /**
+      Not-equal operator.
+
+      @return @c true if @c this and the given SugarAccount are not equal,
+              otherwise @c false
+    */
+    bool operator!=( const SugarAccount & ) const;
+
+    /**
+      Return, if the SugarAccount entry is empty.
+     */
+    bool isEmpty() const;
+
+    /**
+      Clears all entries of the SugarAccount.
+    */
+    void clear();
+
+    /**
+      Set unique identifier.
+     */
+    void setId( const QString &id );
+    /**
+      Return unique identifier.
+     */
+    QString id() const;
+
+    /**
+      Set name.
+     */
+    void setName( const QString &name );
+    /**
+      Return name.
+     */
+    QString name() const;
+
+    /**
+      Set website.
+     */
+    void setWebsite( const QString &value );
+    /**
+      Return Website.
+     */
+    QString website() const;
+
+    /**
+      Set Tycker Symbol.
+     */
+    void setTyckerSymbol( const QString &value );
+    /**
+      Return Tycker Symbol.
+     */
+    QString tyckerSymbol() const;
+
+    /**
+      Set Member of.
+     */
+    void setMemberOf( const QString &value );
+    /**
+      Return Member of.
+     */
+    QString memberOf() const;
+
+    /**
+      Set Ownership.
+     */
+    void setOwnership( const QString &value );
+    /**
+      Return Ownership.
+     */
+    QString ownership() const;
+
+    /**
+      Set Industry.
+     */
+    void setIndustry( const QString &value );
+    /**
+      Return Industry.
+     */
+    QString industry() const;
+
+    /**
+      Set Type.
+     */
+    void setType( const QString &value );
+    /**
+      Return type.
+     */
+    QString type() const;
+
+    /**
+      Set Campaign.
+     */
+    void setCampaign( const QString &value );
+    /**
+      Return Campaign.
+     */
+    QString campaign() const;
+
+    /**
+      Set Assigned to.
+     */
+    void setAssignedTo( const QString &value );
+    /**
+      Return assigned to.
+     */
+    QString assignedTo() const;
+
+    /**
+      Set Office Phone.
+     */
+    void setOfficePhone( const QString &value );
+    /**
+      Return Office Phone.
+     */
+    QString officePhone() const;
+
+    /**
+      Set Fax Phone.
+     */
+    void setFax( const QString &value );
+    /**
+      Return Fax Phone.
+     */
+    QString fax() const;
+
+    /**
+      Set Other Phone.
+     */
+    void setOtherPhone( const QString &value );
+    /**
+      Return Other Phone.
+     */
+    QString otherPhone() const;
+
+    /**
+      Set Employees.
+     */
+    void setEmployees( const QString &value );
+    /**
+      Return Employees.
+     */
+    QString employees() const;
+
+    /**
+      Set Rating.
+     */
+    void setRating( const QString &value );
+    /**
+      Return Rating.
+     */
+    QString rating() const;
+
+    /**
+      Set SIC Code.
+     */
+    void setSicCode( const QString &value );
+    /**
+      Return SIC Code.
+     */
+    QString sicCode() const;
+
+    /**
+      Set Annual Revenue.
+     */
+    void setAnnualRevenue( const QString &value );
+    /**
+      Return AnnualRevenue.
+     */
+    QString annualRevenue() const;
+
+    /**
+      Set Billing Address.
+     */
+    void setBillingAddress( const QString &value );
+    /**
+      Return Billing Address.
+     */
+    QString billingAddress() const;
+
+    /**
+      Set City.
+     */
+    void setCity( const QString &value );
+    /**
+      Return City.
+     */
+    QString city() const;
+
+    /**
+      Set State.
+     */
+    void setState( const QString &value );
+    /**
+      Return State.
+     */
+    QString state() const;
+
+    /**
+      Set Postal Code.
+     */
+    void setPostalCode( const QString &value );
+    /**
+      Return Postal Code.
+     */
+    QString postalCode() const;
+
+    /**
+      Set Country.
+     */
+    void setCountry( const QString &value );
+    /**
+      Return Country.
+     */
+    QString country() const;
+
+    /**
+      Set Shipping Address.
+     */
+    void setShippingAddress( const QString &value );
+    /**
+      Return Shipping Address.
+     */
+    QString shippingAddress() const;
+
+    /**
+      Set Shipping City.
+     */
+    void setShippingCity( const QString &value );
+    /**
+      Return Shipping City.
+     */
+    QString shippingCity() const;
+
+    /**
+      Set Shipping State.
+     */
+    void setShippingState( const QString &value );
+    /**
+      Return Shipping State.
+     */
+    QString shippingState() const;
+
+    /**
+      Set Shipping Postal Code.
+     */
+    void setShippingPostalCode( const QString &value );
+    /**
+      Return Shipping Postal Code.
+     */
+    QString shippingPostalCode() const;
+
+    /**
+      Set Shipping Country.
+     */
+    void setShippingCountry( const QString &value );
+    /**
+      Return Shipping Country.
+     */
+    QString shippingCountry() const;
+
+    /**
+      Set Primary Email.
+     */
+    void setPrimaryEmail( const QString &value );
+    /**
+      Return Primary Email.
+     */
+    QString primaryEmail() const;
+
+    /**
+      Set Opted Out Email.
+     */
+    void setOptedOutEmail( const QString &value );
+    /**
+      Return Opted Out Email.
+     */
+    QString optedOutEmail() const;
+
+    /**
+      Set Invalid Email.
+     */
+    void setInvalidEmail( const QString &value );
+    /**
+      Return Invalid Email.
+     */
+    QString invalidEmail() const;
+
+    /**
+      Set Description.
+     */
+    void setDescription( const QString &value );
+    /**
+      Return Description.
+     */
+    QString description() const;
+
+
+  private:
+    class Private;
+    QSharedDataPointer<Private> d;
+};
+
+#endif /* SUGARACCOUNT_H */
+
