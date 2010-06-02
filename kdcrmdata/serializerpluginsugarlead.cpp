8917268f21a5d9d6801abd34bd887fc36ba976e5
Michel Boyer de la Giroday <michel.giroday@kdab.com>
2010-06-02 10:57:47 +0000

    implemented sugarlead srializer plugin
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@35330 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ kdcrmdata/serializerpluginsugarlead.cpp
@@ -0,0 +1,43 @@
+#include "serializerpluginsugarlead.h"
+
+#include "sugarlead.h"
+#include "sugarleadio.h"
+
+#include <akonadi/item.h>
+
+#include <QtCore/qplugin.h>
+
+using namespace Akonadi;
+
+bool SerializerPluginSugarLead::deserialize( Item& item, const QByteArray& label, QIODevice& data, int version )
+{
+    Q_UNUSED( version );
+
+    if ( label != Item::FullPayload )
+        return false;
+
+    SugarLead sugarLead;
+    SugarLeadIO io;
+    if ( !io.readSugarLead( &data, sugarLead ) )
+        return false;
+
+    item.setPayload<SugarLead>( sugarLead );
+
+    return true;
+}
+
+void SerializerPluginSugarLead::serialize( const Item& item, const QByteArray& label, QIODevice& data, int &version )
+{
+    Q_UNUSED( version );
+
+    if ( label != Item::FullPayload || !item.hasPayload<SugarLead>() )
+        return;
+
+    const SugarLead sugarLead = item.payload<SugarLead>();
+    SugarLeadIO io;
+    io.writeSugarLead( sugarLead, &data );
+}
+
+Q_EXPORT_PLUGIN2( akonadi_serializer_sugarlead, Akonadi::SerializerPluginSugarLead )
+
+#include "serializerpluginsugarlead.moc"
---
+++ kdcrmdata/serializerpluginsugarlead.h
@@ -0,0 +1,23 @@
+#ifndef SERIALIZERPLUGINSUGARLEAD_H
+#define SERIALIZERPLUGINSUGARLEAD_H
+
+#include <QtCore/QObject>
+
+#include <akonadi/itemserializerplugin.h>
+
+namespace Akonadi {
+
+class SerializerPluginSugarAccount : public QObject, public ItemSerializerPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES( Akonadi::ItemSerializerPlugin )
+
+public:
+    bool deserialize( Item& item, const QByteArray& label, QIODevice& data, int version );
+    void serialize( const Item& item, const QByteArray& label, QIODevice& data, int &version );
+};
+
+}
+
+#endif /* SERIALIZERPLUGINSUGARLEAD_H */
+
