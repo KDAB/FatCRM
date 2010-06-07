ced26db4b7d411cb9c94f004c7ef851c17cc77f1
Michel Boyer de la Giroday <michel.giroday@kdab.com>
2010-06-07 06:05:11 +0000

    add the sugarcampaign class
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@35366 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ kdcrmdata/sugarcampaign.cpp
@@ -0,0 +1,497 @@
+#include "sugarcampaign.h"
+
+#include <QtCore/QSharedData>
+#include <QtCore/QString>
+#include <QDebug>
+
+class SugarCampaign::Private : public QSharedData
+{
+  public:
+    Private()
+      : mEmpty( true )
+    {
+
+    }
+
+    Private( const Private &other )
+      : QSharedData( other )
+    {
+      mEmpty = other.mEmpty;
+
+      mId = other.mId;
+      mName = other.mName;
+      mDateEntered = other.mDateEntered;
+      mDateModified = other.mDateModified;
+      mModifiedUserId = other.mModifiedUserId;
+      mModifiedByName = other.mModifiedByName;
+      mCreatedBy = other.mCreatedBy;
+      mCreatedByName = other.mCreatedByName;
+      mDeleted = other.mDeleted;
+      mAssignedUserId = other.mAssignedUserId;
+      mAssignedUserName = other.mAssignedUserName;
+      mTrackerKey = other.mTrackerKey;
+      mTrackerCount = other.mTrackerCount;
+      mReferUrl = other.mReferUrl;
+      mTrackerText = other.mTrackerText;
+      mStartDate = other.mStartDate;
+      mEndDate = other.mEndDate;
+      mStatus = other.mStatus;
+      mImpressions = other.mImpressions;
+      mCurrencyId = other.mCurrencyId;
+      mBudget = other.mBudget;
+      mExpectedCost = other.mExpectedCost;
+      mActualCost = other.mActualCost;
+      mExpectedRevenue = other.mExpectedRevenue;
+      mCampaignType = other.mCampaignType;
+      mObjective = other.mObjective;
+      mContent = other.mContent;
+      mFrequency = other.mFrequency;
+    }
+
+    bool mEmpty;
+
+    QString mId;
+    QString mName;
+    QString mDateEntered;
+    QString mDateModified;
+    QString mModifiedUserId;
+    QString mModifiedByName;
+    QString mCreatedBy;
+    QString mCreatedByName;
+    QString mDeleted;
+    QString mAssignedUserId;
+    QString mAssignedUserName;
+    QString mTrackerKey;
+    QString mTrackerCount;
+    QString mReferUrl;
+    QString mTrackerText;
+    QString mStartDate;
+    QString mEndDate;
+    QString mStatus;
+    QString mImpressions;
+    QString mCurrencyId;
+    QString mBudget;
+    QString mExpectedCost;
+    QString mActualCost;
+    QString mExpectedRevenue;
+    QString mCampaignType;
+    QString mObjective;
+    QString mContent;
+    QString mFrequency;
+};
+
+SugarCampaign::SugarCampaign()
+  : d( new Private )
+{
+}
+
+
+SugarCampaign::SugarCampaign( const SugarCampaign &other )
+  : d( other.d )
+{
+}
+
+SugarCampaign::~SugarCampaign()
+{
+}
+
+SugarCampaign &SugarCampaign::operator=( const SugarCampaign &other )
+{
+  if ( this != &other ) {
+    d = other.d;
+  }
+
+  return *this;
+}
+
+bool SugarCampaign::operator==( const SugarCampaign &other ) const
+{
+    if ( d->mId != other.d->mId )
+        return false;
+    if ( d->mName != other.d->mName )
+        return false;
+    if ( d->mDateEntered != other.d->mDateEntered )
+        return false;
+    if ( d->mDateModified != other.d->mDateModified )
+        return false;
+    if ( d->mModifiedUserId != other.d->mModifiedUserId )
+        return false;
+    if ( d->mModifiedByName != other.d->mModifiedByName )
+        return false;
+    if ( d->mCreatedBy != other.d->mCreatedBy )
+        return false;
+    if ( d->mCreatedByName != other.d->mCreatedByName )
+        return false;
+    if ( d->mDeleted != other.d->mDeleted )
+        return false;
+    if ( d->mAssignedUserId != other.d->mAssignedUserId )
+        return false;
+    if ( d->mAssignedUserName != other.d->mAssignedUserName )
+        return false;
+    if ( d->mTrackerKey != other.d->mTrackerKey )
+        return false;
+    if ( d->mTrackerCount != other.d->mTrackerCount )
+        return false;
+    if ( d->mReferUrl != other.d->mReferUrl )
+        return false;
+    if ( d->mTrackerText != other.d->mTrackerText )
+        return false;
+    if ( d->mStartDate != other.d->mStartDate )
+        return false;
+    if ( d->mEndDate != other.d->mEndDate )
+        return false;
+    if ( d->mStatus != other.d->mStatus )
+        return false;
+    if ( d->mImpressions != other.d->mImpressions )
+        return false;
+    if ( d->mCurrencyId != other.d->mCurrencyId )
+        return false;
+    if ( d->mBudget != other.d->mBudget )
+        return false;
+    if ( d->mExpectedCost != other.d->mExpectedCost )
+        return false;
+    if ( d->mActualCost != other.d->mActualCost )
+        return false;
+    if ( d->mExpectedRevenue != other.d->mExpectedRevenue )
+        return false;
+    if ( d->mCampaignType != other.d->mCampaignType )
+        return false;
+    if ( d->mObjective != other.d->mObjective )
+        return false;
+    if ( d->mContent != other.d->mContent )
+        return false;
+    if ( d->mFrequency != other.d->mFrequency )
+        return false;
+
+  return true;
+}
+
+bool SugarCampaign::operator!=( const SugarCampaign &a ) const
+{
+  return !( a == *this );
+}
+
+bool SugarCampaign::isEmpty() const
+{
+  return d->mEmpty;
+}
+
+void SugarCampaign::clear()
+{
+  *this = SugarCampaign();
+}
+
+void SugarCampaign::setId( const QString &id )
+{
+  d->mEmpty = false;
+  d->mId = id;
+}
+
+QString SugarCampaign::id() const
+{
+  return d->mId;
+}
+
+void SugarCampaign::setName( const QString &name )
+{
+  d->mEmpty = false;
+  d->mName = name;
+}
+
+QString SugarCampaign::name() const
+{
+  return d->mName;
+}
+
+void SugarCampaign::setDateEntered( const QString &value )
+{
+    d->mEmpty = false;
+    d->mDateEntered = value;
+}
+
+QString SugarCampaign::dateEntered() const
+{
+    return d->mDateEntered;
+}
+
+void SugarCampaign::setDateModified( const QString &value )
+{
+    d->mEmpty = false;
+    d->mDateModified = value;
+}
+
+QString SugarCampaign::dateModified() const
+{
+    return d->mDateModified;
+}
+
+void SugarCampaign::setModifiedUserId( const QString &value )
+{
+    d->mEmpty = false;
+    d->mModifiedUserId = value;
+}
+
+QString SugarCampaign::modifiedUserId() const
+{
+    return d->mModifiedUserId;
+}
+
+void SugarCampaign::setModifiedByName( const QString &value )
+{
+    d->mEmpty = false;
+    d->mModifiedByName = value;
+}
+
+QString SugarCampaign::modifiedByName() const
+{
+    return d->mModifiedByName;
+}
+
+void SugarCampaign::setCreatedBy( const QString &value )
+{
+    d->mEmpty = false;
+    d->mCreatedBy = value;
+}
+
+QString SugarCampaign::createdBy() const
+{
+    return d->mCreatedBy;
+}
+
+void SugarCampaign::setCreatedByName( const QString &value )
+{
+    d->mEmpty = false;
+    d->mCreatedByName = value;
+}
+
+QString SugarCampaign::createdByName() const
+{
+    return d->mCreatedByName;
+}
+
+void SugarCampaign::setDeleted( const QString &value )
+{
+    d->mEmpty = false;
+    d->mDeleted = value;
+}
+
+QString SugarCampaign::deleted() const
+{
+    return d->mDeleted;
+}
+
+void SugarCampaign::setAssignedUserId( const QString &value )
+{
+    d->mEmpty = false;
+    d->mAssignedUserId = value;
+}
+
+QString SugarCampaign::assignedUserId() const
+{
+    return d->mAssignedUserId;
+}
+
+void SugarCampaign::setAssignedUserName( const QString &value )
+{
+    d->mEmpty = false;
+    d->mAssignedUserName = value;
+}
+
+QString SugarCampaign::assignedUserName() const
+{
+    return d->mAssignedUserName;
+}
+
+void SugarCampaign::setTrackerKey( const QString &value )
+{
+    d->mEmpty = false;
+    d->mTrackerKey = value;
+}
+
+QString SugarCampaign::trackerKey() const
+{
+    return d->mTrackerKey;
+}
+
+void SugarCampaign::setTrackerCount( const QString &value )
+{
+    d->mEmpty = false;
+    d->mTrackerCount = value;
+}
+
+QString SugarCampaign::trackerCount() const
+{
+    return d->mTrackerCount;
+}
+
+void SugarCampaign::setRefereUrl( const QString &value )
+{
+    d->mEmpty = false;
+    d->mReferUrl = value;
+}
+
+QString SugarCampaign::refereUrl() const
+{
+    return d->mReferUrl;
+}
+
+void SugarCampaign::setTrackerText( const QString &value )
+{
+    d->mEmpty = false;
+    d->mTrackerText = value;
+}
+
+QString SugarCampaign::trackerText() const
+{
+    return d->mTrackerText;
+}
+
+void SugarCampaign::setStartDate( const QString &value )
+{
+    d->mEmpty = false;
+    d->mStartDate = value;
+}
+
+QString SugarCampaign::startDate() const
+{
+    return d->mStartDate;
+}
+
+void SugarCampaign::setEndDate( const QString &value )
+{
+    d->mEmpty = false;
+    d->mEndDate = value;
+}
+
+QString SugarCampaign::endDate() const
+{
+    return d->mEndDate;
+}
+
+void SugarCampaign::setStatus( const QString &value )
+{
+    d->mEmpty = false;
+    d->mStatus = value;
+}
+
+QString SugarCampaign::status() const
+{
+    return d->mStatus;
+}
+
+void SugarCampaign::setImpressions( const QString &value )
+{
+    d->mEmpty = false;
+    d->mImpressions = value;
+}
+
+QString SugarCampaign::impressions() const
+{
+    return d->mImpressions;
+}
+
+void SugarCampaign::setCurrencyId( const QString &value )
+{
+    d->mEmpty = false;
+    d->mCurrencyId = value;
+}
+
+QString SugarCampaign::currencyId() const
+{
+    return d->mCurrencyId;
+}
+
+void SugarCampaign::setBudget( const QString &value )
+{
+    d->mEmpty = false;
+    d->mBudget = value;
+}
+
+QString SugarCampaign::budget() const
+{
+    return d->mBudget;
+}
+
+void SugarCampaign::setExpectedCost( const QString &value )
+{
+    d->mEmpty = false;
+    d->mExpectedCost = value;
+}
+
+QString SugarCampaign::expectedCost() const
+{
+    return d->mExpectedCost;
+}
+
+void SugarCampaign::setActualCost( const QString &value )
+{
+    d->mEmpty = false;
+    d->mActualCost = value;
+}
+
+QString SugarCampaign::actualCost() const
+{
+    return d->mActualCost;
+}
+
+void SugarCampaign::setExpectedRevenue( const QString &value )
+{
+    d->mEmpty = false;
+    d->mExpectedRevenue = value;
+}
+
+QString SugarCampaign::expectedRevenue() const
+{
+    return d->mExpectedRevenue;
+}
+
+void SugarCampaign::setCampaignType( const QString &value )
+{
+    d->mEmpty = false;
+    d->mCampaignType = value;
+}
+
+QString SugarCampaign::campaignType() const
+{
+    return d->mCampaignType;
+}
+
+void SugarCampaign::setObjective( const QString &value )
+{
+    d->mEmpty = false;
+    d->mObjective = value;
+}
+
+QString SugarCampaign::objective() const
+{
+    return d->mObjective;
+}
+
+void SugarCampaign::setContent( const QString &value )
+{
+    d->mEmpty = false;
+    d->mContent = value;
+}
+
+QString SugarCampaign::content() const
+{
+    return d->mContent;
+}
+
+void SugarCampaign::setFrequency( const QString &value )
+{
+    d->mEmpty = false;
+    d->mFrequency = value;
+}
+
+QString SugarCampaign::frequency() const
+{
+    return d->mFrequency;
+}
+
+QString SugarCampaign::mimeType()
+{
+    return QLatin1String( "application/x-vnd.kdab.crm.campaign" );
+}
+
+
---
+++ kdcrmdata/sugarcampaign.h
@@ -0,0 +1,332 @@
+#ifndef SUGARCAMPAIGN_H
+#define SUGARCAMPAIGN_H
+
+#include <QtCore/QSharedDataPointer>
+
+#include "kdcrmdata_export.h"
+
+/**
+ Short: sugar campaign entry
+
+  This class represents an campaign entry in sugarcrm.
+
+  The data of this class is implicitly shared. You can pass this class by value.
+ */
+class KDCRMDATA_EXPORT SugarCampaign
+{
+
+  public:
+
+    /**
+      Construct an empty SugarCampaign entry.
+     */
+    SugarCampaign();
+
+    /**
+      Destroys the SugarCampaign entry.
+     */
+    ~SugarCampaign();
+
+    /**
+      Copy constructor.
+     */
+    SugarCampaign( const SugarCampaign & );
+
+    /**
+      Assignment operator.
+
+      return a reference to this
+    */
+    SugarCampaign &operator=( const SugarCampaign & );
+
+    /**
+      Equality operator.
+
+      return true if this and the given SugarCampaign are equal,
+              otherwise false
+    */
+    bool operator==( const SugarCampaign & ) const;
+
+    /**
+      Not-equal operator.
+
+      return true if  this and the given SugarCampaign are not equal,
+              otherwise false
+    */
+    bool operator!=( const SugarCampaign & ) const;
+
+    /**
+      Return, if the SugarCampaign entry is empty.
+     */
+    bool isEmpty() const;
+
+    /**
+      Clears all entries of the SugarCampaign.
+    */
+    void clear();
+
+    /**
+      Set unique identifier.
+     */
+    void setId( const QString &id );
+    /**
+      Return unique identifier.
+     */
+    QString id() const;
+
+    /**
+      Set the Campaign Name.
+     */
+    void setName( const QString &id );
+    /**
+      Return the Campaign Name.
+     */
+    QString name() const;
+
+    /**
+      Set Creation date.
+     */
+    void setDateEntered( const QString &name );
+    /**
+      Return Creation date.
+     */
+    QString dateEntered() const;
+
+    /**
+      Set Modification date.
+     */
+    void setDateModified( const QString &name );
+    /**
+      Return Modification date.
+     */
+    QString dateModified() const;
+
+    /**
+      Set Modified User id.
+     */
+    void setModifiedUserId( const QString &name );
+    /**
+      Return Modified User id.
+     */
+    QString modifiedUserId() const;
+
+    /**
+      Set Modified By name.
+     */
+    void setModifiedByName( const QString &name );
+    /**
+      Return Modified By name.
+     */
+    QString modifiedByName() const;
+
+    /**
+      Set Created By id.
+     */
+    void setCreatedBy( const QString &name );
+    /**
+      Return created by id.
+     */
+    QString createdBy() const;
+
+    /**
+      Set Created By Name.
+     */
+    void setCreatedByName( const QString &name );
+    /**
+      Return created by Name.
+     */
+    QString createdByName() const;
+
+    /**
+      Set Deleted.
+    */
+    void setDeleted( const QString &value );
+    /**
+      Return Deleted.
+     */
+    QString deleted() const;
+
+    /**
+      Set Assigned User Id.
+    */
+    void setAssignedUserId( const QString &value );
+    /**
+      Return Assigned User Id.
+     */
+    QString assignedUserId() const;
+
+    /**
+      Set Assigned User Name.
+     */
+    void setAssignedUserName( const QString &value );
+    /**
+      Return assigned User Name.
+     */
+    QString assignedUserName() const;
+
+    /**
+      Set the tracker key.
+     */
+    void setTrackerKey( const QString &value );
+    /**
+      Return tracker key.
+     */
+    QString trackerKey() const;
+
+    /**
+      Set tracker count.
+     */
+    void setTrackerCount( const QString &value );
+    /**
+      Return tracker count.
+     */
+    QString trackerCount() const;
+
+    /**
+      Set the reference Url.
+     */
+    void setReferUrl( const QString &value );
+    /**
+      Return the reference Url.
+     */
+    QString referUrl() const;
+
+    /**
+      Set the tracker text.
+     */
+    void setTrackerText( const QString &value );
+    /**
+      Return the tracker text.
+     */
+    QString trackerText() const;
+
+    /**
+      Set the Campaign start date.
+     */
+    void setStartDate( const QString &value );
+    /**
+      Return the Campaign start date.
+     */
+    QString startDate() const;
+
+    /**
+       Set the Campaign end date.
+     */
+    void setEndDate( const QString &value );
+    /**
+      Return the Campaign end date.
+     */
+    QString endDate() const;
+
+    /**
+      Set the Campaign status.
+     */
+    void setStatus( const QString &value );
+    /**
+      Return the Campaing Status.
+     */
+    QString status() const;
+
+    /**
+       Set the Campaign impressions.
+     */
+    void setImpressions( const QString &value );
+    /**
+       Return the Campaign impressions.
+     */
+    QString impressions() const;
+
+    /**
+      Set the Campaign currency id.
+     */
+    void setCurrencyId( const QString &value );
+    /**
+      Return the Campaign currency id.
+     */
+    QString currencyId() const;
+
+    /**
+      Set the Campaign budget.
+     */
+    void setBudget( const QString &value );
+    /**
+      Return the Campaign budget.
+     */
+    QString budget() const;
+
+    /**
+      Set the Campaign expected cost.
+     */
+    void setExpectedCost( const QString &value );
+    /**
+      Return the Campaign expected cost.
+     */
+    QString expectedCost() const;
+
+    /**
+      Set the Campaign actual cost .
+     */
+    void setActualCost( const QString &value );
+    /**
+      Return the Campaign actual cost.
+     */
+    QString actualCost() const;
+
+    /**
+      Set the Campaign expected revenue.
+     */
+    void setExpectedRevenue( const QString &value );
+    /**
+      Return the Campaign expected revenue.
+     */
+    QString expectedRevenue() const;
+
+    /**
+      Set the Campaign type.
+     */
+    void setCampaignType( const QString &value );
+    /**
+      Return the Campaign type.
+     */
+    QString campaignType() const;
+
+    /**
+      Set the Campaign objective.
+     */
+    void setObjective( const QString &value );
+    /**
+      Return the Campaign objective.
+     */
+    QString objective() const;
+
+    /**
+      Set the Campaign content.
+     */
+    void setContent( const QString &value );
+    /**
+      Return the Campaign content.
+     */
+    QString content() const;
+
+    /**
+      Set the Campaign frequency.
+     */
+    void setFrequency( const QString &value );
+    /**
+      Return Primary Address Postal code.
+     */
+    QString frequency() const;
+
+    /**
+       Return the Mime type
+     */
+    static QString mimeType();
+
+  private:
+    class Private;
+    QSharedDataPointer<Private> d;
+};
+
+
+#endif /* SUGARCAMPAIGN_H */
+
