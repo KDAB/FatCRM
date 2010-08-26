265dda46ef94a223691d45b578c9665382449d6b
Michel Boyer de la Giroday <michel.giroday@kdab.com>
2010-08-26 13:57:42 +0000

    beginning of modifyLeads - not finished yet
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@36685 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- tests/squish/suite_SugarCrm1/tst_modifyLead/test.py
+++ tests/squish/suite_SugarCrm1/tst_modifyLead/test.py
@@ -1,43 +1,68 @@
-
 def main():
     startApplication("sugarclient")
-    # load an existing account 
-    clickTab(waitForObject(":SugarCRM Client: admin@SugarCRM on localhost.qt_tabwidget_tabbar_QTabBar"), "Leads")
-    mouseClick(waitForObject(":Form.searchLE_QLineEdit_3"), 128, 10, 0, Qt.LeftButton)
-    type(waitForObject(":Form.searchLE_QLineEdit_3"), "test account")
-    clickButton(waitForObject(":SugarCRM Client: admin@SugarCRM on localhost.Show Details_QCheckBox"))
-    # modify a few values
-    mouseClick(waitForObject(":Details.campaignName_QComboBox_3"), 59, 10, 0, Qt.LeftButton)
-    mouseClick(waitForObjectItem(":Details.campaignName_QComboBox_3", "test campaign"), 36, 7, 0, Qt.LeftButton)
-    mouseClick(waitForObjectItem(":Details.salutation_QComboBox", "Mr\\."), 46, 6, 0, Qt.LeftButton)
-    mouseDrag(waitForObject(":Details.firstName_QLineEdit"), 20, 14, 46, 0, 1, Qt.LeftButton)
-    type(waitForObject(":Details.firstName_QLineEdit"), "Pigasse")
-    mouseClick(waitForObject(":Other Details.status_QComboBox"), 97, 16, 0, Qt.LeftButton)
-    mouseClick(waitForObjectItem(":Other Details.status_QComboBox", "Converted"), 90, 9, 0, Qt.LeftButton)
-    mouseDrag(waitForObject(":Description:.description_QTextEdit_3"), 2, 11, 157, 5, 1, Qt.LeftButton)
-    type(waitForObject(":Description:.description_QTextEdit_3"), "converted status")
-    # save 
-    clickButton(waitForObject(":&Lead Details.Save_QPushButton")) 
-    activateItem(waitForObjectItem(":SugarCRM Client: admin@SugarCRM on localhost.menubar_QMenuBar", "File"))
-    activateItem(waitForObjectItem(":SugarCRM Client: admin@SugarCRM on localhost.File_QMenu", "Syncronize"))
-    label = waitForObject(':Lead Information.createdDate_QLabel')
+    # load methods     
+    import campaignsHandling
+    import leadsHandling
+    
+    # create a campaign
+    # data to be registered
+    name = "TestCampaign" 
+    status = "In Queue"
+    detailsList = [name, status, "1/1", "2/2", "Radio", "US Dollars : $", "100000", "200000"]
+    otherDetailsList = ["max", "20000", "85000", "100000", "radio marketting"]      
+    description = "a campaign created via my refactored functions"
+    # launch the details widget
+    campaignsHandling.createCampaign()
+    # write the data
+    campaignsHandling.registerDetails(detailsList)
+    campaignsHandling.registerOtherDetails(otherDetailsList)       
+    campaignsHandling.registerDescription(description)
+    # save remotely
+    campaignsHandling.saveCampaign()
+    # make sure it is saved before proceeding 
+    label = findObject(':Campaign Information.modifiedDate_QLineEdit')
     waitFor("label.text !=''")
-    # data is back from remote client check modified values
-    waitFor("object.exists(':Details.campaignName_QComboBox_3')")
-    test.compare(findObject(":Details.campaignName_QComboBox_3").currentText, "test campaign")
-    waitFor("object.exists(':Details.salutation_QComboBox')")
-    test.compare(findObject(":Details.salutation_QComboBox").currentText, "Mr.")
-    waitFor("object.exists(':Details.firstName_QLineEdit')")
-    test.compare(findObject(":Details.firstName_QLineEdit").text, "asse")
-    waitFor("object.exists(':Other Details.status_QComboBox')")
-    test.compare(findObject(":Other Details.status_QComboBox").currentText, "Converted")
-    waitFor("object.exists(':Description:.description_QTextEdit_3')")
-    test.compare(findObject(":Description:.description_QTextEdit_3").plainText, "converted status")
-    clickButton(waitForObject(":Form.New Lead_QPushButton"))
-    clickButton(waitForObject(":Form.Clear_QToolButton_3"))
-    mouseClick(waitForObject(":Form.searchLE_QLineEdit_3"), 329, 10, 0, Qt.LeftButton)
-    type(waitForObject(":Form.searchLE_QLineEdit_3"), "test acc")
-    waitForObjectItem(":Form.leadsTV_Akonadi::EntityTreeView", "max")
-    clickItem(":Form.leadsTV_Akonadi::EntityTreeView", "max", 15, 5, 0, Qt.LeftButton)
+    # data to be registered
+    firstName = "Lead" 
+    detailsList = ["Self Generated", "This is a self generated lead", "", "Michel", "Mr.", firstName, "Bob", "2/2", "an account", "VD", "Sales", "max"]
+    otherDetailsList = ["New", "this is a new lead", "150000", "123456", "789012", "345678", "901234", "567890", "true", "michel@kdab.com", "mic@kdab.se"]
+    addressesList = ["jonas väg", "Hagfors", " a state","32456", "Sweden", "jonsons", "väg", "Örebro", "45678", "USA", "false"]   
+    description = "a lead created via my refactored functions"
+    # launch the details widget
+    leadsHandling.createLead()
+    # write the data
+    leadsHandling.registerDetails(detailsList)
+    leadsHandling.registerOtherDetails(otherDetailsList)
+    leadsHandling.registerAddresses(addressesList)    
+    leadsHandling.registerDescription(description)
+    # save remotely
+    leadsHandling.saveLead()
+    # make sure it is saved before proceeding
+    label = findObject(':Lead Information.modifiedDate_QLineEdit')
+    waitFor("label.text !=''")
+    # modified data     
+    mdetailsList = ["Cold Call", "mThis is a self generated lead", "TestCampaign", "mMichel", "Mrs.", "mLead", "mBob", "3/3", "mMMMMan account", "mVD", "mSales", "max"]
+    motherDetailsList = ["Assigned", "mmmmMthis is a new lead", "190000", "m123456", "m789012", "m345678", "m901234", "m567890", "false", "mmmmichel@kdab.com", "mmmic@kdab.se"]
+    maddressesList = ["mmjonas väg", "mHagfors", " ma state","m32456", "mSweden", "mjonsons", "mväg", "mÖrebro", "m45678", "mUSA", "false"]   
+    mdescription = "mmmmmMa lead created via my refactored functions"
+    # write the data
+    leadsHandling.registerDetails(mdetailsList)
+    leadsHandling.registerOtherDetails(motherDetailsList)
+    leadsHandling.registerAddresses(maddressesList)    
+    leadsHandling.registerDescription(mdescription)
+    # save remotely
+    leadsHandling.saveLead()
+    # make sure it is saved before proceeding
+    label = findObject(':Lead Information.modifiedDate_QLineEdit')
+    waitFor("label.text !=''")
+    # check data values 
+    leadsHandling.checkDetailsValues(mdetailsList)
+    leadsHandling.checkOtherDetailsValues(motherDetailsList)
+    leadsHandling.checkAddressesValues(maddressesList)   
+    leadsHandling.checkDescriptionValue(mdescription)
+    # remove 
+    campaignsHandling.removeCampaign(name)  
+    leadsHandling.removeLead(firstName)
+    # quit
     activateItem(waitForObjectItem(":SugarCRM Client: admin@SugarCRM on localhost.menubar_QMenuBar", "File"))
     activateItem(waitForObjectItem(":SugarCRM Client: admin@SugarCRM on localhost.File_QMenu", "Quit"))
