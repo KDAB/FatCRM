4db46464b1d16491372dc24e6d8a1f61af55daa9
Kevin Krammer <kevin.krammer@kdab.com>
2010-09-06 15:51:45 +0000

    Add job for listing entries of a module
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@36828 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ resources/sugarcrm/listentriesjob.cpp
@@ -0,0 +1,87 @@
+#include "listentriesjob.h"
+
+#include "modulehandler.h"
+#include "sugarsoap.h"
+
+#include <KDSoapMessage.h>
+
+#include <akonadi/collection.h>
+
+#include <KDebug>
+
+#include <QStringList>
+
+using namespace Akonadi;
+
+class ListEntriesJob::Private
+{
+    ListEntriesJob *const q;
+
+public:
+    explicit Private( ListEntriesJob *parent ) : q( parent )
+    {
+    }
+
+public:
+    Collection mCollection;
+    ModuleHandler *mHandler;
+
+public: // slots
+    void listEntriesDone( const TNS__Get_entry_list_result &callResult );
+    void listEntriesError( const KDSoapMessage &fault );
+};
+
+void ListEntriesJob::Private::listEntriesDone( const TNS__Get_entry_list_result &callResult )
+{
+    if ( callResult.result_count() > 0 ) {
+        const Item::List items =
+            mHandler->itemsFromListEntriesResponse( callResult.entry_list(), mCollection );
+        kDebug() << "List Entries for" << mHandler->moduleName()
+                 << "received" << items.count() << "items";
+        emit q->itemsReceived( items );
+    } else {
+        kDebug() << "List Entries for" << mHandler->moduleName() << "done";
+        q->emitResult();
+    }
+}
+
+void ListEntriesJob::Private::listEntriesError( const KDSoapMessage &fault )
+{
+    if ( !q->handleLoginError( fault ) ) {
+        kWarning() << "List Entries Error:" << fault.faultAsString();
+
+        q->setError( SugarJob::SoapError );
+        q->setErrorText( fault.faultAsString() );
+        q->emitResult();
+    }
+}
+
+ListEntriesJob::ListEntriesJob( SugarSession *session, QObject *parent )
+    : SugarJob( session, parent ), d( new Private( this ) )
+{
+    connect( soap(), SIGNAL( get_entry_listDone( TNS__Get_entry_list_result ) ),
+             this,  SLOT( listEntriesDone( TNS__Get_entry_list_result ) ) );
+    connect( soap(), SIGNAL( get_entry_listError( KDSoapMessage ) ),
+             this,  SLOT( listEntriesError( KDSoapMessage ) ) );
+}
+
+ListEntriesJob::~ListEntriesJob()
+{
+    delete d;
+}
+
+void ListEntriesJob::setModule( const Akonadi::Collection &collection, ModuleHandler *handler )
+{
+    d->mCollection = collection;
+    d->mHandler = handler;
+}
+
+void ListEntriesJob::startSugarTask()
+{
+    Q_ASSERT( d->mCollection.isValid() );
+    Q_ASSERT( d->mHandler != 0 );
+
+    d->mHandler->listEntries( 0, soap(), sessionId() );
+}
+
+#include "listentriesjob.moc"
---
+++ resources/sugarcrm/listentriesjob.h
@@ -0,0 +1,41 @@
+#ifndef LISTENTRIESJOB_H
+#define LISTENTRIESJOB_H
+
+#include "sugarjob.h"
+
+#include <akonadi/item.h>
+
+namespace Akonadi {
+    class Collection;
+}
+
+class ModuleHandler;
+class QStringList;
+class TNS__Get_entry_list_result;
+
+class ListEntriesJob : public SugarJob
+{
+    Q_OBJECT
+
+public:
+    explicit ListEntriesJob( SugarSession *session, QObject *parent = 0 );
+
+    ~ListEntriesJob();
+
+    void setModule( const Akonadi::Collection &collection, ModuleHandler *handler );
+
+Q_SIGNALS:
+    void itemsReceived( const Akonadi::Item::List &items );
+
+protected:
+    void startSugarTask();
+
+private:
+    class Private;
+    Private *const d;
+
+    Q_PRIVATE_SLOT( d, void listEntriesDone( const TNS__Get_entry_list_result &callResult ) )
+    Q_PRIVATE_SLOT( d, void listEntriesError( const KDSoapMessage &fault ) )
+};
+
+#endif
