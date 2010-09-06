af93c0d9aa11638b54b2d98556c00d8a2ba3ad1f
Kevin Krammer <kevin.krammer@kdab.com>
2010-09-06 14:55:00 +0000

    Add job for listing modules
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@36827 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ resources/sugarcrm/listmodulesjob.cpp
@@ -0,0 +1,73 @@
+#include "listmodulesjob.h"
+
+#include "sugarsoap.h"
+
+#include <KDSoapMessage.h>
+
+#include <KDebug>
+
+#include <QStringList>
+
+class ListModulesJob::Private
+{
+    ListModulesJob *const q;
+
+public:
+    explicit Private( ListModulesJob *parent ) : q( parent )
+    {
+    }
+
+public:
+    QStringList mModules;
+
+public: // slots
+    void listModulesDone( const TNS__Module_list &callResult );
+    void listModulesError( const KDSoapMessage &fault );
+};
+
+void ListModulesJob::Private::listModulesDone( const TNS__Module_list &callResult )
+{
+    const TNS__Select_fields moduleNames = callResult.modules();
+    mModules = moduleNames.items();
+
+    kDebug() << "Got" << mModules.count() << "available modules";
+
+    q->emitResult();
+}
+
+void ListModulesJob::Private::listModulesError( const KDSoapMessage &fault )
+{
+    if ( !q->handleLoginError( fault ) ) {
+        kWarning() << "List Modules Error:" << fault.faultAsString();
+
+        q->setError( SugarJob::SoapError );
+        q->setErrorText( fault.faultAsString() );
+        q->emitResult();
+    }
+}
+
+ListModulesJob::ListModulesJob( SugarSession *session, QObject *parent )
+    : SugarJob( session, parent ), d( new Private( this ) )
+{
+    connect( soap(), SIGNAL( get_available_modulesDone( TNS__Module_list ) ),
+             this,  SLOT( listModulesDone( TNS__Module_list ) ) );
+    connect( soap(), SIGNAL( get_available_modulesError( KDSoapMessage ) ),
+             this,  SLOT( listModulesError( KDSoapMessage ) ) );
+}
+
+ListModulesJob::~ListModulesJob()
+{
+    delete d;
+}
+
+QStringList ListModulesJob::modules() const
+{
+    return d->mModules;
+}
+
+void ListModulesJob::startSugarTask()
+{
+    soap()->asyncGet_available_modules( sessionId() );
+}
+
+#include "listmodulesjob.moc"
---
+++ resources/sugarcrm/listmodulesjob.h
@@ -0,0 +1,31 @@
+#ifndef LISTMODULESJOB_H
+#define LISTMODULESJOB_H
+
+#include "sugarjob.h"
+
+class QStringList;
+class TNS__Module_list;
+
+class ListModulesJob : public SugarJob
+{
+    Q_OBJECT
+
+public:
+    explicit ListModulesJob( SugarSession *session, QObject *parent = 0 );
+
+    ~ListModulesJob();
+
+    QStringList modules() const;
+
+protected:
+    void startSugarTask();
+
+private:
+    class Private;
+    Private *const d;
+
+    Q_PRIVATE_SLOT( d, void listModulesDone( const TNS__Module_list &callResult ) )
+    Q_PRIVATE_SLOT( d, void listModulesError( const KDSoapMessage &fault ) )
+};
+
+#endif
