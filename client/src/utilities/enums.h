455fef7b6731cbfc0db574bf0dd3a57bf1947d14
Kevin Krammer <kevin.krammer@kdab.com>
2010-10-25 15:41:53 +0000

    Add manager class for cross referenced data and model class for feeding it into comboboxes
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@37671 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- client/src/utilities/enums.h
+++ client/src/utilities/enums.h
@@ -2,6 +2,7 @@
 #define ENUMS_H
 
 enum DetailsType {Account, Opportunity, Lead, Contact, Campaign };
+enum ReferencedDataType { AccountRef, AssignedToRef, CampaignRef, ReportsToRef };
 
 #endif /* ENUMS_H */
 
