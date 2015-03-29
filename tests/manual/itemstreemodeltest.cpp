8bd5a514c4bbb74c598c0e9a2f6901a2064948ec
David Faure <david.faure@kdab.com>
2015-03-29 12:50:01 +0000

    make the test work, by setting up the model after we know the root collection
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@61644 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- tests/manual/itemstreemodeltest.cpp
+++ tests/manual/itemstreemodeltest.cpp
@@ -6,10 +6,72 @@
 #include <KLocale>
 
 #include <QTreeView>
+#include <QTimer>
+#include <Akonadi/Collection>
+#include <Akonadi/CollectionFetchJob>
 #include <Akonadi/ChangeRecorder>
 #include <Akonadi/ItemFetchScope>
+#include <Akonadi/EntityTreeView>
+#include <Akonadi/EntityMimeTypeFilterModel>
+#include <Akonadi/Session>
 #include <itemstreemodel.h>
 #include <sugaraccount.h>
+#include <sugaropportunity.h>
+
+using namespace Akonadi;
+
+class Controller : public QObject
+{
+    Q_OBJECT
+public:
+    Controller(QTreeView *tree, ChangeRecorder *changeRecorder)
+        : mChangeRecorder(changeRecorder),
+          mTreeView(tree)
+    {
+        CollectionFetchJob *fetchJob = new CollectionFetchJob(Collection::root(), CollectionFetchJob::Recursive,
+                                                              changeRecorder->session());
+        fetchJob->fetchScope().setContentMimeTypes(QStringList() << SugarOpportunity::mimeType());
+       // fetchJob->fetchScope().setAncestorRetrieval(Akonadi::CollectionFetchScope::All); // needed?
+
+        connect(fetchJob, SIGNAL(result(KJob*)),
+                this, SLOT(rootFetchJobDone(KJob*)));
+    }
+private Q_SLOTS:
+    void rootFetchJobDone(KJob *job) {
+        if (job->error()) {
+            kWarning() << job->errorString();
+            return;
+        }
+        CollectionFetchJob *collectionJob = qobject_cast<CollectionFetchJob *>(job);
+        const Collection::List list = collectionJob->collections();
+
+        kDebug() << list.count();
+        kDebug() << list.first().name();
+        mChangeRecorder->setCollectionMonitored(list.first(), true);
+
+        ItemsTreeModel *model = new ItemsTreeModel(Opportunity, mChangeRecorder);
+        mTreeView->setModel(model);
+
+
+        //
+    #if 1
+        Akonadi::EntityMimeTypeFilterModel *filterModel = new Akonadi::EntityMimeTypeFilterModel(this);
+        filterModel->setSourceModel(model);
+        filterModel->addMimeTypeInclusionFilter(SugarOpportunity::mimeType());
+        filterModel->setHeaderGroup(Akonadi::EntityTreeModel::ItemListHeaders);
+
+        Akonadi::EntityTreeView *view = new Akonadi::EntityTreeView;
+        view->setModel(filterModel);
+        view->resize(900, 900);
+        view->show();
+    #endif
+
+    }
+
+private:
+    ChangeRecorder *mChangeRecorder;
+    QTreeView *mTreeView;
+};
 
 int main(int argc, char **argv)
 {
@@ -22,18 +84,17 @@ int main(int argc, char **argv)
     KApplication app;
 
     Akonadi::ChangeRecorder recorder;
-    recorder.setMimeTypeMonitored(SugarAccount::mimeType());
+
+    recorder.setMimeTypeMonitored(SugarOpportunity::mimeType());
     recorder.itemFetchScope().fetchFullPayload(true);
 
-    ItemsTreeModel *model = new ItemsTreeModel(Account, &recorder);
-
-    QTreeView *tree = new QTreeView;
-    tree->setModel(model);
+    QTreeView *tree = new QTreeView(0);
     tree->resize(900, 900);
     tree->show();
+
+    Controller controller(tree, &recorder);
 
     return app.exec();
 }
 
-
-
+#include "itemstreemodeltest.moc"
