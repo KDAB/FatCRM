af2887ffa651bee13e1da9cc988426901a2d0d2e
David Faure <david.faure@kdab.com>
2015-05-04 14:02:46 +0000

    faster startup, load first the items we can see upfront
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/projects/FatCRM/trunk@62400 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- client/src/utilities/collectionmanager.cpp
+++ client/src/utilities/collectionmanager.cpp
@@ -25,14 +25,43 @@ void CollectionManager::setResource(const QByteArray &identifier)
             this, SLOT(slotCollectionFetchResult(KJob*)));
 }
 
+static bool collectionLessThan(const Collection &c1, const Collection &c2)
+{
+    // In my tests contentMimeTypes always had exactly one entry.
+    const QString contentMimeType1 = c1.contentMimeTypes().first();
+    const QString contentMimeType2 = c2.contentMimeTypes().first();
+
+    // Now emit them in the right order for fast startup.
+    // We want to see Opportunities first, and load Notes last.
+
+    static const char s_opp[] = "application/x-vnd.kdab.crm.opportunity";
+    if (contentMimeType1 == s_opp)
+        return true;
+    if (contentMimeType2 == s_opp)
+        return false;
+
+    static const char s_note[] = "application/x-vnd.kdab.crm.note";
+    if (contentMimeType1 == s_note)
+        return false;
+    if (contentMimeType2 == s_note)
+        return true;
+
+    return contentMimeType1 < contentMimeType2;
+}
+
 void CollectionManager::slotCollectionFetchResult(KJob *job)
 {
     CollectionFetchJob *fetchJob = qobject_cast<CollectionFetchJob *>(job);
 
+    QVector<Collection> collections;
     Q_FOREACH (const Collection &collection, fetchJob->collections()) {
+        collections.append(collection);
+    }
+
+    qSort(collections.begin(), collections.end(), collectionLessThan);
+
+    Q_FOREACH (const Collection &collection, collections) {
         //qDebug() << collection.contentMimeTypes() << "name" << collection.name();
-        // In my tests contentMimeTypes always had exactly one entry.
-        const QString contentMimeType = collection.contentMimeTypes().first();
-        emit collectionResult(contentMimeType, collection);
+        emit collectionResult(collection.contentMimeTypes().first(), collection);
     }
 }
