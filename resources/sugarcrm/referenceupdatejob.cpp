1f09935e5e7fb9c9d3c72dce7e218bd3b623712c
David Faure <david.faure@kdab.com>
2015-06-23 11:10:23 +0200

    add missing files

---
+++ resources/sugarcrm/referenceupdatejob.cpp
@@ -0,0 +1,69 @@
+/*
+  This file is part of FatCRM, a desktop application for SugarCRM written by KDAB.
+
+  Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Authors: David Faure <david.faure@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "referenceupdatejob.h"
+
+#include <akonadi/itemfetchjob.h>
+#include <akonadi/itemfetchscope.h>
+#include <akonadi/itemmodifyjob.h>
+
+ReferenceUpdateJob::ReferenceUpdateJob(const Akonadi::Collection &collection, QObject *parent) :
+    KCompositeJob(parent),
+    mCollection(collection)
+{
+}
+
+void ReferenceUpdateJob::start()
+{
+    Akonadi::ItemFetchJob *job = new Akonadi::ItemFetchJob(mCollection, this);
+    job->fetchScope().setCacheOnly(true);
+    job->fetchScope().fetchFullPayload(true);
+    connect(job, SIGNAL(itemsReceived(Akonadi::Item::List)), this, SLOT(slotItemsReceived(Akonadi::Item::List)));
+    addSubjob(job);
+}
+
+void ReferenceUpdateJob::slotItemsReceived(const Akonadi::Item::List &items)
+{
+    Akonadi::Item::List modifiedItems;
+    foreach (const Akonadi::Item &item, items) {
+        // My kingdom for a C++ std::function here instead of a virtual
+        Akonadi::Item copy = item;
+        if (updateItem(copy)) {
+            modifiedItems.append(copy);
+        }
+    }
+    if (!modifiedItems.isEmpty()) {
+        // This method can be called multiple times, so we can create multiple modify jobs
+        Akonadi::ItemModifyJob *modifyJob = new Akonadi::ItemModifyJob(modifiedItems, this);
+        addSubjob(modifyJob);
+    }
+}
+
+void ReferenceUpdateJob::slotResult(KJob *job)
+{
+    KCompositeJob::slotResult(job); // does error handling
+
+    if (!job->error() && qobject_cast<Akonadi::ItemModifyJob *>(job)) {
+        if (subjobs().isEmpty()) {
+            emitResult();
+        }
+    }
+}
+
---
+++ resources/sugarcrm/referenceupdatejob.h
@@ -0,0 +1,55 @@
+/*
+  This file is part of FatCRM, a desktop application for SugarCRM written by KDAB.
+
+  Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Authors: David Faure <david.faure@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef REFERENCEUPDATEJOB_H
+#define REFERENCEUPDATEJOB_H
+
+#include <KCompositeJob>
+#include <Akonadi/Collection>
+#include <Akonadi/Item>
+
+/**
+ * @brief The reference update job goes through a collection and changes
+ * all references from A to B. Example: renaming an account means changing
+ * the account name in all opportunities for this account.
+ */
+class ReferenceUpdateJob : public KCompositeJob
+{
+    Q_OBJECT
+public:
+    explicit ReferenceUpdateJob(const Akonadi::Collection &collection, QObject *parent = 0);
+
+    void start() Q_DECL_OVERRIDE;
+
+protected:
+    virtual bool updateItem(Akonadi::Item &item) = 0;
+
+signals:
+
+private slots:
+    void slotItemsReceived(const Akonadi::Item::List &items);
+    void slotResult( KJob *job ) Q_DECL_OVERRIDE;
+
+private:
+    Akonadi::Collection mCollection;
+};
+
+#endif // REFERENCEUPDATEJOB_H
+
