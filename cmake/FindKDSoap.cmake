3a0bf44d6efc5f327b4e8bbe72aacf5ebe9ecd0d
Allen Winter <allen.winter@kdab.com>
2015-06-28 12:37:11 -0400

    FindKDSoap.cmake - revert looking in the KDSoapClient include prefix

--- cmake/FindKDSoap.cmake
+++ cmake/FindKDSoap.cmake
@@ -19,12 +19,6 @@ find_library(KDSoap_LIBRARIES
 find_path(KDSoap_INCLUDE_DIR
   NAMES KDSoap KDSoapValue.h
 )
-if(NOT KDSoap_INCLUDE_DIR)
-  find_path(KDSoap_INCLUDE_DIR
-    NAMES KDSoap KDSoapValue.h
-    PATH_SUFFIXES KDSoapClient
-  )
-endif()
 find_program(KDSoap_CODEGENERATOR
   NAMES kdwsdl2cpp
   PATH_SUFFIXES bin
