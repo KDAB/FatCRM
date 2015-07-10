46a6f4c1830e9c5e8b06ad7a81e9fedf0d29d118
Allen Winter <allen.winter@kdab.com>
2015-07-10 15:19:36 -0400

    kdcrmdata/enumdefinitions - add KDAB license and copyright

--- kdcrmdata/enumdefinitions.h
+++ kdcrmdata/enumdefinitions.h
@@ -1,11 +1,33 @@
+/*
+  This file is part of FatCRM, a desktop application for SugarCRM written by KDAB.
+
+  Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Authors: David Faure <david.faure@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef ENUMDEFINITIONS_H
 #define ENUMDEFINITIONS_H
 
-#include <vector>
-#include <QMap>
-#include <QString>
-#include <QMetaType>
 #include "kdcrmdata_export.h"
+
+#include <vector>
+
+#include <QMap>
+#include <QMetaType>
+#include <QString>
 
 /**
  * @brief The EnumDefinitions class lists enum definitions for one type of item.
@@ -13,7 +35,8 @@
  * salutation, lead_source, and portal_user_type
  *
  * Each enum definition contains a list of values (ID and display string).
- * E.g. one of the values in the 'lead source' definition is: ID="QtDevDays", DisplayString="Qt Developer Days"
+ * E.g. one of the values in the 'lead source' definition is:
+ *   ID="QtDevDays", DisplayString="Qt Developer Days"
  */
 class KDCRMDATA_EXPORT EnumDefinitions
 {
