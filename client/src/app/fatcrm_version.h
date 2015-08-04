c66115b56d1f9a775a522ab9cbcb6b50754192fb
David Faure <david.faure@kdab.com>
2015-08-04 11:17:35 +0200

    bump version to 1.0.8

--- client/src/app/fatcrm_version.h
+++ client/src/app/fatcrm_version.h
@@ -1,6 +1,6 @@
 #ifndef FATCRM_VERSION_H
 #define FATCRM_VERSION_H
 
-static const char s_version[] = "1.0.7 (released July 06 2015)";
+static const char s_version[] = "1.0.8 (released August 04 2015)";
 
 #endif
