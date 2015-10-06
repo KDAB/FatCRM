31782497ce5db9f4986febb84e005720f47be6d4
Allen Winter <allen.winter@kdab.com>
2015-10-06 11:54:56 -0400

    FindKDSoap.cmake - also look for KDSoap headers in the KDSoap prefix

--- cmake/FindKDSoap.cmake
+++ cmake/FindKDSoap.cmake
@@ -19,6 +19,10 @@ find_library(KDSoap_LIBRARIES
 find_path(KDSoap_INCLUDE_DIR
   NAMES KDSoap KDSoapValue.h
 )
+find_path(KDSoap_INCLUDE_DIR
+  NAMES KDSoap KDSoapValue.h
+  PATH_SUFFIXES KDSoapClient
+)
 find_program(KDSoap_CODEGENERATOR
   NAMES kdwsdl2cpp
   PATH_SUFFIXES bin
