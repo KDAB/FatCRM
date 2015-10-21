13d7f865fb48bfd7fdb530520ba575be7ac8a2eb
David Faure <david.faure@kdab.com>
2015-10-21 11:58:32 +0200

    FATCRM-90: handle removal and opp-change for notes and emails

--- client/src/utilities/notesrepository.cpp
+++ client/src/utilities/notesrepository.cpp
@@ -80,15 +80,40 @@ void NotesRepository::slotNotesReceived(const Akonadi::Item::List &items)
 
 void NotesRepository::storeNote(const Akonadi::Item &item)
 {
-    //kDebug() << item.id() << item.mimeType() << ;
     if (item.hasPayload<SugarNote>()) {
         SugarNote note = item.payload<SugarNote>();
+        const QString id = note.id();
+        Q_ASSERT(!id.isEmpty());
+        removeNote(id); // handle change of opp
         if (note.parentType() == QLatin1String("Opportunities")) {
-            mNotesHash[note.parentId()].append(note);
+            const QString parentId = note.parentId();
+            if (!parentId.isEmpty()) {
+                mNotesHash[parentId].append(note);
+                mNotesParentIdHash.insert(id, parentId);
+            } else {
+                mNotesParentIdHash.remove(id);
+            }
         } else {
             // We also get notes for Accounts and Emails.
             // (well, no longer, we filter this out in the resource)
             //kDebug() << "ignoring notes for" << note.parentType();
+        }
+    }
+}
+
+void NotesRepository::removeNote(const QString &id)
+{
+    Q_ASSERT(!id.isEmpty());
+    const QString oldParentId = mNotesParentIdHash.value(id);
+    if (!oldParentId.isEmpty()) {
+        kDebug() << "note" << id << "oldParentId" << oldParentId;
+        // Note is no longer associated with this opportunity
+        QVector<SugarNote> &notes = mNotesHash[oldParentId];
+        auto it = std::find_if(notes.begin(), notes.end(), [id](const SugarNote &n) { return n.id() == id; });
+        if (it != notes.end()) {
+            const int idx = std::distance(notes.begin(), it);
+            kDebug() << "Removing note at" << idx;
+            notes.remove(idx);
         }
     }
 }
@@ -119,6 +144,10 @@ void NotesRepository::monitorChanges()
     configureItemFetchScope(mMonitor->itemFetchScope());
     connect(mMonitor, SIGNAL(itemAdded(Akonadi::Item,Akonadi::Collection)),
             this, SLOT(slotItemAdded(Akonadi::Item,Akonadi::Collection)));
+    connect(mMonitor, SIGNAL(itemRemoved(Akonadi::Item)),
+            this, SLOT(slotItemRemoved(Akonadi::Item)));
+    connect(mMonitor, SIGNAL(itemChanged(Akonadi::Item,QSet<QByteArray>)),
+            this, SLOT(slotItemChanged(Akonadi::Item,QSet<QByteArray>)));
 }
 
 QVector<SugarEmail> NotesRepository::emailsForOpportunity(const QString &id) const
@@ -139,15 +168,40 @@ void NotesRepository::slotEmailsReceived(const Akonadi::Item::List &items)
 
 void NotesRepository::storeEmail(const Akonadi::Item &item)
 {
-    //kDebug() << item.id() << item.mimeType() << ;
     if (item.hasPayload<SugarEmail>()) {
         SugarEmail email = item.payload<SugarEmail>();
+        const QString id = email.id();
+        Q_ASSERT(!id.isEmpty());
+        removeEmail(id); // handle change of opp
         if (email.parentType() == QLatin1String("Opportunities")) {
-            mEmailsHash[email.parentId()].append(email);
+            const QString parentId = email.parentId();
+            if (!parentId.isEmpty()) {
+                mEmailsHash[parentId].append(email);
+                mEmailsParentIdHash.insert(id, parentId);
+            } else {
+                mEmailsParentIdHash.remove(id);
+            }
         } else {
             // We also get emails for Accounts and Emails.
             // (well, no longer, we filter this out in the resource)
             //kDebug() << "ignoring emails for" << email.parentType();
+        }
+    }
+}
+
+void NotesRepository::removeEmail(const QString &id)
+{
+    Q_ASSERT(!id.isEmpty());
+    const QString oldParentId = mEmailsParentIdHash.value(id);
+    if (!oldParentId.isEmpty()) {
+        kDebug() << "email" << id << "oldParentId" << oldParentId;
+        // Email is no longer associated with this opportunity
+        QVector<SugarEmail> &emails = mEmailsHash[oldParentId];
+        auto it = std::find_if(emails.begin(), emails.end(), [id](const SugarEmail &n) { return n.id() == id; });
+        if (it != emails.end()) {
+            const int idx = std::distance(emails.begin(), it);
+            kDebug() << "Removing email at" << idx;
+            emails.remove(idx);
         }
     }
 }
@@ -159,15 +213,44 @@ void NotesRepository::configureItemFetchScope(Akonadi::ItemFetchScope &scope)
     scope.fetchFullPayload(true);
 }
 
-void NotesRepository::slotItemAdded(const Akonadi::Item &item, const Akonadi::Collection &collection)
+void NotesRepository::updateItem(const Akonadi::Item &item, const Akonadi::Collection &collection)
 {
     if (collection == mNotesCollection) {
-        //kDebug() << item.id() << item.mimeType();
         storeNote(item);
     } else if (collection == mEmailsCollection) {
         storeEmail(item);
     } else {
-        kWarning() << "Unexpected collection" << collection << ", expected" << mNotesCollection.id();
-        return;
+        kWarning() << "Unexpected collection" << collection << ", expected" << mNotesCollection.id() << "or" << mEmailsCollection.id();
     }
+}
+
+void NotesRepository::slotItemAdded(const Akonadi::Item &item, const Akonadi::Collection &collection)
+{
+    kDebug() << item.id() << item.mimeType();
+    updateItem(item, collection);
+}
+
+void NotesRepository::slotItemRemoved(const Akonadi::Item &item)
+{
+    kDebug() << item.id();
+    const Akonadi::Collection collection = item.parentCollection();
+    // Don't use the payload to handle removals (no payload anymore on removal)
+    if (collection == mNotesCollection) {
+        removeNote(item.remoteId());
+    } else if (collection == mEmailsCollection) {
+        removeEmail(item.remoteId());
+    } else {
+        kWarning() << "Unexpected collection" << collection << ", expected" << mNotesCollection.id() << "or" << mEmailsCollection.id();
+    }
+}
+
+void NotesRepository::slotItemChanged(const Akonadi::Item &item, const QSet<QByteArray> &partIdentifiers)
+{
+    // I get only REMOTEREVISION even when changing the parentid in the SugarEmail...
+    //kDebug() << item.id() << partIdentifiers;
+    Q_UNUSED(partIdentifiers);
+
+    // Handle the case where the parent id changed
+    const Akonadi::Collection collection = item.parentCollection();
+    updateItem(item, collection);
 }
