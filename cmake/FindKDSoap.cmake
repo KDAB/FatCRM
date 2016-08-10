69d32574362a5695dd54b8fa02f2117f8c08d28b
Allen Winter <allen.winter@kdab.com>
2016-08-10 17:54:13 -0400

    buildsystem - allow building with kdreports and kdsoap OBS packages which are build with autogen and don't provide cmake configurations

---
+++ cmake/FindKDSoap.cmake
@@ -0,0 +1,32 @@
+# - Find KDSoap
+# This module finds if KDSoap is installed.
+#
+# KDSoap_FOUND          - Set to TRUE if KDSoap was found.
+# KDSoap_LIBRARIES      - Path to KDSoap libraries.
+# KDSoap_INCLUDE_DIR    - Path to the KDSoap include directory.
+# KDSoap_CODEGENERATOR  - Path to the KDSoap code generator.
+#
+# Copyright (C) 2011-2016 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+
+# Redistribution and use is allowed according to the terms of the BSD license
+
+include(FindPackageHandleStandardArgs)
+
+find_library(KDSoap_LIBRARIES
+  NAMES KDSoap kdsoap
+  PATH_SUFFIXES lib bin
+  HINTS ENV KDSOAPDIR
+)
+find_path(KDSoap_INCLUDE_DIR
+  NAMES KDSoapClient KDSoapGlobal
+  PATH_SUFFIXES include
+  HINTS ENV KDSOAPDIR
+)
+find_program(KDSoap_CODEGENERATOR
+  NAMES kdwsdl2cpp
+  PATH_SUFFIXES bin
+)
+
+mark_as_advanced(KDSoap_LIBRARIES KDSoap_INCLUDE_DIR KDSoap_CODEGENERATOR)
+
+find_package_handle_standard_args(KDSoap DEFAULT_MSG KDSoap_LIBRARIES KDSoap_INCLUDE_DIR KDSoap_CODEGENERATOR)
